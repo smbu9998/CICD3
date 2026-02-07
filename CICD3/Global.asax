30cf3f2714520dba4e06170e8fcfdf3ceb5be570
Sameera <sameera@optimosoftware.co.uk>
2026-02-08 00:50:12 +0530

    first commit

---
+++ CICD3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CICD3.MvcApplication" Language="C#" %>
---
+++ CICD3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CICD3.Startup))]
+namespace CICD3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
